566de933e9aba8152ce97422eb96eefedb383108
Thoon Thoon <thoonlay779@gmail.com>
2026-08-11 15:14:41 +0630

    current process id

---
+++ database/migrations/2026_08_11_150338_add_workflow_id_and_current_process_id_to_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('workflow_id')->unsigned()->nullable();
+            $table->integer('current_process_id')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('workflow_id');
+            $table->dropColumn('current_process_id');
+        });
+    }
+};
