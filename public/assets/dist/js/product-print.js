7181cbcb074526f22e3f8da5ef94d93a2eed118d
Thoon Thoon <thoonlay779@gmail.com>
2026-09-17 14:08:31 +0630

    add lines

--- public/assets/dist/js/product-print.js
+++ public/assets/dist/js/product-print.js
@@ -1,44 +1,6 @@
 console.log("product print js");
 
-// document.querySelectorAll('.sheet-description').forEach(function (container) {
 
-//     const lines = [
-//         ...container.querySelectorAll('.description-line')
-//     ];
-
-//     // container.innerHTML = '';
-
-//     let lineCount = 0;
-//     const descriptionLineLimit = Number(
-//         container.dataset.descriptionLineLimit
-//     );
-//     // console.log(descriptionLineLimit);
-//     for (const line of lines) {
-//         console.log(line);
-
-//         line.textContent = line.textContent.trim();
-//         line.style.whiteSpace = 'normal';
-
-//         console.log(line.scrollWidth , line.clientWidth);
-//         if (line.scrollWidth > line.clientWidth) {
-//             console.log('ဒီ line က မဆံ့ပါ:', line.textContent);
-//             lineCount += 1;
-//         }
-//         lineCount += 1;
-
-//         if(lineCount > descriptionLineLimit){
-//             line.style.display = "none"
-//         }
-//     }
-// });
-
-// document.querySelectorAll('.description-line').forEach(function (line) {
-//     if (line.scrollWidth > line.clientWidth) {
-//         line.classList.add('truncated');
-
-//         console.log('ဒီ line က မဆံ့ပါ:', line.textContent);
-//     }
-// });
 
 document.querySelectorAll('.sheet-description').forEach(function (container) {
     const lines = [
@@ -49,21 +11,28 @@ document.querySelectorAll('.sheet-description').forEach(function (container) {
     const descriptionLineLimit = Number(
         container.dataset.descriptionLineLimit
     );
-    lines.forEach(function (line) {
-        console.log(lineCount , descriptionLineLimit)
+    lines.forEach(function (line,idx) {
         if(lineCount > descriptionLineLimit){
             // line.style.display = 'none';
             line.remove();
             return;
         }
 
-        lineCount++;
+        if(idx == lines.length -1 ) return;
+
         if (line.scrollWidth > line.clientWidth) {
             console.log('ဒီ line က မဆံ့ပါ:', line.textContent);
-            lineCount += 1;
-        }
+            let lineHeight = line.clientHeight;
+            console.log(lineHeight);
 
-        line.textContent = line.textContent.trim();
-        line.style.whiteSpace = 'normal';
+            line.style.whiteSpace = 'normal';
+            line.textContent = line.textContent.trim();
+
+            let addLines = Math.floor(line.scrollHeight / lineHeight);
+            console.log(addLines);
+            lineCount += addLines;
+        }else{
+            lineCount++;
+        }
     });
 });
